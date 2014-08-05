3274466a8a1ab0ef5e6f8610f1e10b9a4780d86a
Garrett Flanagan <ecomoperations@gmail.com>
2014-08-05 09:54:20 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Abridgr::Application.config.secret_key_base = 'ad40de0cab4bf4b933386062dd2a78ce8b705293dfe962fc5a613d684a572490e2d4673919654ddcf104d84aa48c7bf6cdaea150e812cac44cd8bd47a0743c2a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Abridgr::Application.config.session_store :cookie_store, key: '_abridgr_session'
