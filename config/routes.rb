0f80236fa40b8291c97b9ee68ea82dd5e1cf536f
Garrett Flanagan <ecomoperations@gmail.com>
2014-08-12 15:16:30 -0400

    added post page and link

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Abridgr::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
+  get 'welcome/topics'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
