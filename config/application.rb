cba91e6f2d749dc205044a5c6626db808c0172fe
Garrett Flanagan <ecomoperations@gmail.com>
2014-10-04 16:40:42 -0400

    one last

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Abridgr
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-  config.assets.precompile += %w( '*.svg', '*.eot', '*.woff', '*.ttf' )
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
