8426d69d3efcdaee70dacf486e35b5b10228aca7
Garrett Flanagan <ecomoperations@gmail.com>
2014-12-14 11:00:21 -0500

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,11 +33,10 @@ Abridgr::Application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
-<<<<<<< HEAD
+
   #trying everything
   
-=======
->>>>>>> parent of 83c81a4... more deleting
+
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
