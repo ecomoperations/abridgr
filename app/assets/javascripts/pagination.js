ffb845325b43b5e036cbd6be5c350b34fc726812
Garrett Flanagan <ecomoperations@gmail.com>
2014-12-21 11:28:23 -0500

    reverted changes manually

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -16,20 +16,3 @@ jQuery(function() {
   }
 });
 
-var days, today;
-jQuery(function() {
-  if ($('#infinite-scrolling2').size() > 0) {
-    console.log("I'm here")
-    $(window).on('scroll', function() {
-      console.log("I'm also here")
-      var more_posts_url;
-      more_posts_url = $('.pagination .next_page').attr('href');
-      console.log(more_posts_url)
-      if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
-        console.log("this is where i want to be")
-        $('.pagination').html('<img src="/assets/images/ajax-loader.gif" alt="Loading..." title="Loading..." />');
-        $.getScript(more_posts_url);
-      }
-    });
-  }
-});
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-  	@topics = Topic.paginate(page: params[:page], per_page: 5).order('created_at DESC')
+  	# @topics = Topic.paginate(page: params[:page], per_page: 5).order('created_at DESC')
+    @topics = Topic.all.order('created_at DESC')
 
     @posts = Post.paginate(page: params[:page], per_page: 4).order('created_at DESC')
   	respond_to do |format|
