5e7ba583d4e194a63a11da0f670d0bd7020e575a
Garrett Flanagan <ecomoperations@gmail.com>
2014-12-21 19:18:39 -0500

    added topics to welcome topics and changed photo and css background

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   	# @topics = Topic.paginate(page: params[:page], per_page: 5).order('created_at DESC')
-    @topics = Topic.all.order('created_at DESC')
+    @topics = Topic.all.order('created_at DESC').first(5)
 
     @posts = Post.paginate(page: params[:page], per_page: 4).order('created_at DESC')
   	respond_to do |format|
