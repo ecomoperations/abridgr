27a212fee3bed817639436ad081173916c0f1c8e
Garrett Flanagan <ecomoperations@gmail.com>
2015-01-03 12:58:08 -0500

    ajax gif chage in js

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -9,7 +9,7 @@ jQuery(function() {
       console.log(more_posts_url)
       if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
         console.log("this is where i want to be")
-        $('.pagination').html('<img src="/assets/images/ajax-loader.gif" alt="Loading..." title="Loading..." />');
+        $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />');
         $.getScript(more_posts_url);
       }
     });
